af2f4cf759256ed5ac9bfc0198014ced0d587871
azizuddinrahil <azizuddin.rahil123@gmail.com>
2022-03-14 19:59:59 +0530

    Added game logic

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
-'use strict';
+"use strict";
 
 // console.log(document.querySelector('.message'));
 
 // document.querySelector('.message').textContent = 'Something';
 
-const number = Math.trunc(Math.random() * 20) + 1;
-document.querySelector('.number').textContent = number;
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector(".number").textContent = secretNumber;
 
-document.querySelector('.check').addEventListener('click', function () {
-  const guess = Number(document.querySelector('.guess').value);
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value);
   console.log(guess, typeof guess);
 
   if (!guess) {
-    document.querySelector('.message').textContent = 'No Number given!';
-  } else {
-    document.querySelector('.message').textContent = 'Nice one';
+    document.querySelector(".message").textContent = "No Number given!";
+  } else if (guess === secretNumber) {
+    document.querySelector(".message").textContent = "Nice one 🥳";
+  } else if (guess > secretNumber) {
+    document.querySelector(".message").textContent = "Number is too high 🙄";
+  } else if (guess < secretNumber) {
+    document.querySelector(".message").textContent = "Number is too low";
   }
 });
