1b413fd40a4379bde8a5cd1d9a3bd800acb4f820
azizuddinrahil <azizuddin.rahil123@gmail.com>
2022-03-17 12:06:32 +0530

    Added the highscore feature

--- script.js
+++ script.js
@@ -6,6 +6,7 @@
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20; // Max Score is 20
+let highScore = 0;
 
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
@@ -17,9 +18,14 @@ document.querySelector(".check").addEventListener("click", function () {
   } else if (guess === secretNumber) {
     document.querySelector(".message").textContent = "Nice one 🥳";
     //If the user has the right guess
+    document.querySelector(".number").textContent = secretNumber;
+
     document.querySelector("body").style.backgroundColor = "#60b347";
     document.querySelector(".number").style.width = "30rem";
-    document.querySelector(".number").textContent = secretNumber;
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector(".highscore").textContent = highScore;
+    }
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector(".message").textContent = "Number is too high 🙄";
