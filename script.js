5784f6319c0b6ccd8e05104460881ea6fe731062
azizuddinrahil <azizuddin.rahil123@gmail.com>
2022-03-17 10:26:57 +0530

    Added background color and hid the secret number from the user

--- script.js
+++ script.js
@@ -5,8 +5,8 @@
 // document.querySelector('.message').textContent = 'Something';
 
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
-let score = 20;
-document.querySelector(".number").textContent = secretNumber;
+let score = 20; // Max Score is 20
+//document.querySelector(".number").textContent = secretNumber;
 
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
@@ -14,24 +14,32 @@ document.querySelector(".check").addEventListener("click", function () {
 
   if (!guess) {
     document.querySelector(".message").textContent = "No Number given!";
+    //If there is no number given
   } else if (guess === secretNumber) {
     document.querySelector(".message").textContent = "Nice one 🥳";
+    //If the user has the right guess
+    document.querySelector("body").style.backgroundColor = "#60b347";
+    document.querySelector(".number").style.width = "30rem";
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector(".message").textContent = "Number is too high 🙄";
       score = score - 1;
+      //If the user has wrong guess(Higher guess)
       document.querySelector(".score").textContent = score;
     } else {
       document.querySelector(".message").textContent = "You Lose! 🙁";
+      //If the user lost
       document.querySelector(".score").textContent = 0;
     }
   } else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector(".message").textContent = "Number is too low";
+      //If the user has wrong guess(Low guess)
       score = score - 1;
       document.querySelector(".score").textContent = score;
     } else {
       document.querySelector(".message").textContent = "You Lose! 🙁";
+      //If the user lost
       document.querySelector(".score").textContent = 0;
     }
   }
