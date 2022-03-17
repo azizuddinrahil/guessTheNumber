bddbe1446cd76546f0c5dfc364b4912716d087c3
azizuddinrahil <azizuddin.rahil123@gmail.com>
2022-03-17 10:01:52 +0530

    Updated the game logic and minor bugs fixed

--- script.js
+++ script.js
@@ -5,19 +5,34 @@
 // document.querySelector('.message').textContent = 'Something';
 
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
 document.querySelector(".number").textContent = secretNumber;
 
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
-  console.log(guess, typeof guess);
+  //console.log(guess, typeof guess);
 
   if (!guess) {
     document.querySelector(".message").textContent = "No Number given!";
   } else if (guess === secretNumber) {
     document.querySelector(".message").textContent = "Nice one 🥳";
   } else if (guess > secretNumber) {
-    document.querySelector(".message").textContent = "Number is too high 🙄";
+    if (score > 1) {
+      document.querySelector(".message").textContent = "Number is too high 🙄";
+      score = score - 1;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "You Lose! 🙁";
+      document.querySelector(".score").textContent = 0;
+    }
   } else if (guess < secretNumber) {
-    document.querySelector(".message").textContent = "Number is too low";
+    if (score > 1) {
+      document.querySelector(".message").textContent = "Number is too low";
+      score = score - 1;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "You Lose! 🙁";
+      document.querySelector(".score").textContent = 0;
+    }
   }
 });
